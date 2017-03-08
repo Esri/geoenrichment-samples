bf06a71f37ea9537354ca0224cee64a560fd2d8a
Max Sattarov <msattarov@esri.com>
2017-03-07 18:10:25 -0800

    Added sample requests for custom reports

--- Python/CreateReportGP/Sample Requests.py
+++ Python/CreateReportGP/Sample Requests.py
@@ -30,7 +30,7 @@ response.json()["helperServices"]["geoenrichment"]["url"]
 ge_url = response.json()["helperServices"]["geoenrichment"]["url"]
 
 
-*******************************************
+#*******************************************
 
 response = requests.post(ge_url + "/Geoenrichment/Countries", data)
 
@@ -39,11 +39,11 @@ response.json()["countries"][20]
 response.json()["countries"][20]["id"]
 
 
-******************************************
+#******************************************
 
 response = requests.post(ge_url + "/Geoenrichment/Reports/BG", data)
 
-******************************************
+#******************************************
 import functools
 def get_request_as_string(query_url, data):
 	get_request_data = [k + "=" + str(v) for k, v in data.items()]
@@ -51,8 +51,8 @@ def get_request_as_string(query_url, data):
 
 get_request_as_string(bg_reports_url, data)
 		
-**************************************
-
+#**************************************
+# Custom Reports samples
 
 search_data = {"f":"json", 
     "token":token, 
@@ -64,3 +64,11 @@ search_data = {"f":"json",
     }
 response = requests.post(portal + "/sharing/rest/search", search_data)
 
+
+report_data = {"f":"json",
+    "token":token,
+    "report":'{"itemid":"305c391fdbda4ee49414fc095432ef8c"}',
+    "studyAreas":'[{"featureSet":{"fields":[{"name":"OBJECTID","alias":"Object ID","type":"esriFieldTypeOID"},{"name":"AREA_DESC","alias":"AREA_DESC","type":"esriFieldTypeString","length":8,"nullable":true},{"name":"AREA_DESC2","alias":"AREA_DESC2","type":"esriFieldTypeString","length":19,"nullable":true}],"spatialReference":{"wkid":102100,"latestWkid":3857},"geometryType":"esriGeometryPolygon","features":[{"geometry":{"rings":[[[-13046162.4581862,4038479.8221407],[-13046028.5581138,4038475.17549524],[-13045895.2964613,4038461.25772474],[-13045763.3085999,4038438.13522072],[-13044428.7885244,4037406.11887813],[-13046162.4581862,4046281.68115603]]],"spatialReference":{"wkid":102100,"latestWkid":3857}},"attributes":{"AREA_DESC":"5 milhas","AREA_DESC2":"Anéis: 5 milha raio","OBJECTID":3}}]}}]'}
+
+response = requests.post(ge_url + "/Geoenrichment/CreateReport", report_data)
+    
